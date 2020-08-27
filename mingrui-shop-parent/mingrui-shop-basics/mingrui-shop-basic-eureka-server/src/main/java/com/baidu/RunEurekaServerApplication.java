d48cbd7da903ea3cc40d7aa15b339d76af290eaf
wangzehui111 <zhazhahui316@163.com>
2020-08-27 15:48:39 +0800

    wangzehui

--- mingrui-shop-parent/mingrui-shop-basics/mingrui-shop-basic-eureka-server/src/main/java/com/baidu/RunEurekaServerApplication.java
+++ mingrui-shop-parent/mingrui-shop-basics/mingrui-shop-basic-eureka-server/src/main/java/com/baidu/RunEurekaServerApplication.java
@@ -1,5 +1,6 @@
 package com.baidu;
 
+import io.micrometer.core.instrument.util.JsonUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -16,6 +17,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class RunEurekaServerApplication {
 
     public static void main(String[] args) {
+        System.out.println("111");
         SpringApplication.run(RunEurekaServerApplication.class);
     }
 }
